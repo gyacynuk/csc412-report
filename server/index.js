a33b11fa1ba3b07c55552103c732d6d3b324aed6
Griffin <gyacynuk@gmail.com>
2021-04-21 03:16:14 -0400

    Serve index.html by default, regardless of request

--- server/index.js
+++ server/index.js
@@ -9,7 +9,14 @@ const port = process.env.PORT || 5000
 
 // Configure static middleware to serve from the "./interactive-report/build" directory
 const absolutePath = path.join(__dirname, '..', 'interactive-report', 'build')
+
+
+// Static routes of webpages
 app.use(express.static(absolutePath))
+// Route error handling (such as 404s) is built in to the front-end, so serve the React app for any page requested
+app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, path.join(absolutePath, 'index.html')))
+})
 
 // Start the server
 app.listen(port, () => {
